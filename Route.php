6d072b170827b2d424238accbd2e73b3dfc71647
imzeon <shaojin.du@outlook.com>
2017-04-21 17:10:01 +0800

    调整

--- Route.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Framework\Core\Route;
-
-Route::get('/', function () {
-    echo '路由加载成功！';
-});
-
-Route::get('/test', 'IndexController@index');
---
+++ framework/Core/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jasper
+ * Date: 2017/4/21 0021
+ * Time: 15:44
+ */
+
+namespace Framework\Core;
+
+
+class Exception
+{
+
+}
